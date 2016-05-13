e2981d1eba44aa4db68f6113ff18b09e3243bc88
perrin <stanislas.perrin@inist.fr>
2016-05-13 14:15:15 +0200

    Première version du du script « INIST » -- ToDo P1 du 13 mai 2016

---
+++ libs/inist.lib.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+########################################################################
+#
+# INIST-TOOLS / INIST LIB
+# 
+# Bibliothèque de fonctions standards utilisées par le lanceur ou les
+# outils « INIST-TOOLS ».
+#
+# @author : INIST-CNRS/DPI
+#
+########################################################################
+
+#-----------------------------------------------------------------------
+# Logging
+# $1 : level/priorité (NOTICE, WARNING, ERROR, etc.)
+# $2 : contenu du message ou de l'erreur
+#-----------------------------------------------------------------------
+function IT_MESSAGE {
+  MESSAGE="[INIST-TOOLS] [$1] $2"
+  # echo "$MESSAGE"
+  logger -s -p "$1" "$MESSAGE"
+}
+
+#-----------------------------------------------------------------------
+# Message d'accueil
+#-----------------------------------------------------------------------
+function IT_GREETING {
+  echo "$MODULE_NAME $MODULE_VERSION"
+}
+
+#-----------------------------------------------------------------------
+# Affichage de la version
+#-----------------------------------------------------------------------
+function IT_SHOW_VERSION {
+  echo "$MODULE_NAME est en version $MODULE_VERSION"
+}
+
+#-----------------------------------------------------------------------
+# Affichage de l'aide
+#-----------------------------------------------------------------------
+function IT_SHOW_HELP {
+  echo "Options : "
+  echo -e "\t-v | --version\t\tAffiche la version"
+}
---
+++ tools/inist_proxy.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+########################################################################
+#
+# INIST-TOOLS / INIST_PROXY
+# 
+# Positionn les variables d'environnement de la session courante
+# pour les proxies INIST.
+#
+# @author : INIST-CNRS/DPI
+#
+########################################################################
+
+#-----------------------------------------------------------------------
+# Environnement
+#-----------------------------------------------------------------------
+MODULE_NAME="INIST PROXY"
+MODULE_VERSION=$(git describe --tags)
+CURDIR=$( cd "$( dirname "$0" )" && pwd )
+DIR_MODULE=$(readlink -f "$CURDIR")
+DIR_LIBS=$(readlink -f "$DIR_MODULE/../libs/")
+
+#-----------------------------------------------------------------------
+# Chargement des libs (pour rendre le module autonome)
+#-----------------------------------------------------------------------
+if [ -f "$DIR_LIBS/inist.lib.sh" ]; then
+  source "$DIR_LIBS/inist.lib.sh"
+else
+  logger -s "[$MODULE_NAME] [ERROR] Impossible de charger la librairie depuis « $DIR_LIBS ». Fin."
+  exit 1  
+fi
+
+# Exclusion du proxy
+IT_MESSAGE "NOTICE" "Dépositionnement du proxy pour localhost"
+export no_proxy=localhost,127.0.0.0/8,*.local
+export NO_PROXY=localhost,127.0.0.0/8,*.local
+
+# HTTP
+IT_MESSAGE "NOTICE" "Positionnement du proxy INIST pour HTTPS"
+export http_proxy="http://proxyout.inist.fr:8080"
+export HTTP_PROXY="http://proxyout.inist.fr:8080"
+
+# HTTPS
+IT_MESSAGE "NOTICE" "Positionnement du proxy INIST pour HTTPS"
+export https_proxy="http://proxyout.inist.fr:8080"
+export HTTPS_PROXY="http://proxyout.inist.fr:8080"
+
+# FTP
+IT_MESSAGE "NOTICE" "Positionnement du proxy INIST pour FTP"
+export ftp_proxy="http://proxyout.inist.fr:8080"
+export FTP_PROXY="http://proxyout.inist.fr:8080"
+
+# Soertie "propre"
+exit 0
