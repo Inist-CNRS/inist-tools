d17057a407c2e99da88cf636ca47e74994bbf26b
perrin <stanislas.perrin@inist.fr>
2016-06-01 11:31:21 +0200

    Ajout du script permettant de synchroniser l'heure du PC

---
+++ tools/timesync.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+################################################################################
+#
+# inist-tools / tools / timesync.sh
+#
+# Met à l'heure le PC en synchronisant au serveur de temps de l'INIST
+#
+# @author : INIST-CNRS/DPI
+#
+################################################################################
+
+#-------------------------------------------------------------------------------
+# Environnement
+#-------------------------------------------------------------------------------
+MODULE_NAME="PREPARE-DEB"
+MODULE_DESC="Prépare l'arborescence et les fichiers en vue de créer le .deb"
+MODULE_VERSION=$(git describe --tags)
+MODULE_VERSION_SHORT=$(git describe --tags | cut -d"-" -f1 )
+CURDIR=$( cd "$( dirname "$0" )" && pwd )
+DIR_MODULE=$(readlink -f "$CURDIR")
+DIR_ROOT=$(readlink -f "$DIR_MODULE/..")
+DIR_CONF=$(readlink -f "$DIR_MODULE/../conf")
+DIR_LIBS=$(readlink -f "$DIR_MODULE/../libs")
+DIR_TOOLS=$(readlink -f "$DIR_MODULE/../tools")
+DIR_INSTALL=$(readlink -f "$DIR_MODULE/../install")
+DIR_SYSINSTALL="$DIR_INSTALL/opt"
+
+#-------------------------------------------------------------------------------
+# Mise-à-l'heure
+#-------------------------------------------------------------------------------
+ntpdate ntp-int.inist.fr
