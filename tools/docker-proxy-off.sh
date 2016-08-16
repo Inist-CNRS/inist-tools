978d55c13f2acbabe18a6115cf611b5645fef694
perrin <stanislas.perrin@inist.fr>
2016-08-16 18:26:04 +0200

    Màj des procédures docker-proxy-on et docker-proxy-off

--- tools/docker-proxy-off.sh
+++ tools/docker-proxy-off.sh
@@ -22,10 +22,15 @@ TMPFILE="/run/shm/tmpfile$$"
 # ------------------------------------------------------------------------------
 grep -vi "# inist-tools" "$DOCKER_DEFAULT_FILE" > "$TMPFILE"
 cp "$TMPFILE" "$DOCKER_DEFAULT_FILE"
+rm "$TMPFILE"
 
 # ------------------------------------------------------------------------------
 # Service
 # ------------------------------------------------------------------------------
+
+# Debian + Ubunu >= 15.04 -> systemd
+# Ubuntu 14.04 -> upstart + fichiers sysvinit
+
 is_debian=$(cat /etc/issue | grep -i "debian")
 is_ubuntu=$(cat /etc/issue | grep -i "ubuntu")
 
@@ -37,14 +42,36 @@ else
   platform="unknown"
 fi
 
+# Suppression de la conf spécifique au proxy INIST
+rm /etc/systemd/system/docker.service.d/http-proxy.conf
+
 if [ $platform == "debian" ]; then
   cp /etc/systemd/system/docker.service.d/http-proxy.conf /etc/systemd/system/docker.service.d/http-proxy.conf_inis-tools-backup
-  grep -vi "# inist-tools" /etc/systemd/system/docker.service.d/http-proxy.conf > "$TMPFILE"
-  cp "$TMPFILE" /etc/systemd/system/docker.service.d/http-proxy.conf
+  # cp "$TMPFILE" /etc/systemd/system/docker.service.d/http-proxy.conf
+  systemctl daemon-reload
+  sleep 1
+  systemctl restart docker
+  sleep 1
+  /etc/init.d/docker restart
 elif [ $platform == "ubuntu" ]; then
-  cp /lib/systemd/system/docker.service /lib/systemd/system/docker.service_inist-tools-backup
-  grep -vi "# inist-tools" /lib/systemd/system/docker.service > "$TMPFILE"
-  cp "$TMPFILE" /lib/systemd/system/docker.service
+  ubuntuVersion=$(cat /etc/lsb-release | grep -i "DISTRIB_RELEASE" | cut -d"=" -f 2)
+  _it_std_consoleMessage "CHECK" "Ubuntu « $ubuntuVersion » détecté"
+  case $ubuntuVersion in
+    "12.04" | "12.10" | "13.04" | "13.10" | "14.04" | "14.10" )
+      _it_std_consoleMessage "CHECK" "Utilisation de « update-rc.d »"
+      sudo update-rc.d docker defaults
+      sleep 1
+      service docker restart
+    ;;
+    * )
+      _it_std_consoleMessage "CHECK" "Utilisation de « systemctl »"
+      systemctl daemon-reload
+      sleep 1
+      systemctl restart docker
+      sleep 1
+      /etc/init.d/docker restart
+    ;;
+  esac
 fi
 
 # ------------------------------------------------------------------------------
--- tools/docker-proxy-on.sh
+++ tools/docker-proxy-on.sh
@@ -18,7 +18,7 @@ DOCKER_DEFAULT_FILE="/etc/default/docker"
 
 # PROXY
 INIST_HTTP_PROXY="http://proxyout.inist.fr:8080"
-INIST_HTTPS_PROXY="https://proxyout.inist.fr:8080"
+INIST_HTTPS_PROXY="http://proxyout.inist.fr:8080"
 INIST_FTP_PROXY="http://proxyout.inist.fr:8080"
 INIST_PROXYPAC="http://proxypac.intra.inist.fr/proxy.pac"
 INIST_NO_PROXY="localhost,127.0.0.0/8,*.local,172.16.0.0/16"
@@ -56,28 +56,54 @@ printf "export https_proxy=\"$INIST_HTTPS_PROXY\" # inist-tools\n" >> "$DOCKER_D
 is_debian=$(cat /etc/issue | grep -i "debian")
 is_ubuntu=$(cat /etc/issue | grep -i "ubuntu")
 
-if [ ! -z "$is_debian" ]; then
+if [ "$is_debian" ]; then
   platform="debian"
-elif [ ! -z "$is_ubuntu" ]; then
+elif [ "$is_ubuntu" ]; then
   platform="ubuntu"
 else
   platform="unknown"
 fi
 
+mkdir /etc/systemd/system/docker.service.d/
+touch /etc/systemd/system/docker.service.d/http-proxy.conf
+echo "# inist-tools" >> /etc/systemd/system/docker.service.d/http-proxy.conf
+echo "[Service]" >> /etc/systemd/system/docker.service.d/http-proxy.conf
+echo "EnvironmentFile=/etc/default/docker" >> /etc/systemd/system/docker.service.d/http-proxy.conf
+
 if [ "$platform" == "debian" ]; then
-  # Backup
-  mkdir /etc/systemd/system/docker.service.d/
-  touch /etc/systemd/system/docker.service.d/http-proxy.conf
-  echo "# inist-tools" >> /etc/systemd/system/docker.service.d/http-proxy.conf
-  echo "[Service] # inist-tools" >> /etc/systemd/system/docker.service.d/http-proxy.conf
-  echo "EnvironmentFile=/etc/default/docker # inist-tools" >> /etc/systemd/system/docker.service.d/http-proxy.conf
+  systemctl daemon-reload
+  sleep 1
+  systemctl restart docker
+  sleep 1
   /etc/init.d/docker restart
 elif [ "$platform" == "ubuntu" ]; then
-  touch /lib/systemd/system/docker.service
-  echo "# inist-tools" >> /lib/systemd/system/docker.service
-  echo "[Service] # inist-tools" >> /lib/systemd/system/docker.service
-  echo "EnvironmentFile=/etc/default/docker # inist-tools" >> /lib/systemd/system/docker.service
-  echo "ExecStart=/usr/bin/docker daemon $DOCKER_OPTS -H fd:// # inist-tools" >> /lib/systemd/system/docker.service
+  #
+  echo "ExecStart=/usr/bin/docker daemon \$DOCKER_OPTS -H fd:// # inist-tools" >> /etc/systemd/system/docker.service.d/http-proxy.conf
+  #
+  ubuntuVersion=$(cat /etc/lsb-release | grep -i "DISTRIB_RELEASE" | cut -d"=" -f 2)
+  _it_std_consoleMessage "CHECK" "Ubuntu « $ubuntuVersion » détecté"
+  case $ubuntuVersion in
+    "12.04" | "12.10" | "13.04" | "13.10" | "14.04" | "14.10" )
+      _it_std_consoleMessage "CHECK" "Utilisation de « update-rc.d »"
+      sudo update-rc.d docker defaults
+      sleep 1
+      service docker restart
+    ;;
+    * )
+      _it_std_consoleMessage "CHECK" "Utilisation de « systemctl »"
+      systemctl daemon-reload
+      sleep 1
+      systemctl restart docker
+      sleep 1
+      /etc/init.d/docker restart
+    ;;
+  esac
+#  touch /lib/systemd/system/docker.service
+#  echo "# inist-tools" >> /lib/systemd/system/docker.service
+#  echo "[Service] # inist-tools" >> /lib/systemd/system/docker.service
+#  echo "EnvironmentFile=/etc/default/docker # inist-tools" >> /lib/systemd/system/docker.service
+#  echo "ExecStart=/usr/bin/docker daemon \$DOCKER_OPTS -H fd:// # inist-tools" >> /lib/systemd/system/docker.service
+  
 fi
 
 # Temporisation
