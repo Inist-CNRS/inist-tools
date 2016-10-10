46314da8094fd402dcebd3597bf43333845b08a6
perrin <stanislas.perrin@inist.fr>
2016-10-10 15:20:32 +0200

    Ajout de la gestion du registry INIST pour Docker

---
+++ tools/docker-registry-off.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+################################################################################
+#
+# inist-tools / tools / docker-registry-off.sh
+# 
+# Sous-routine qui supprime le registry INIST pour DOCKER
+#
+# /!\ S'execute en ROOT
+#
+# @author : INIST-CNRS/DPI
+#
+###############################################################################
+
+# ------------------------------------------------------------------------------
+# Variables globales
+# ------------------------------------------------------------------------------
+DOCKER_DEFAULT_FILE="/etc/default/docker"
+INIST_CERT_FILE="/etc/docker/certs.d/vsregistry.intra.inist.fr:5000"
+
+rm -Rf "$INIST_CERT_FILE"
+
