a91e559a024979bb6ddf5216c3b52b7a3148efed
perrin <stanislas.perrin@inist.fr>
2016-11-21 10:12:04 +0100

    Bug sur l'évaluation de la version du kernel dans docker-install

--- tools/docker-install.sh
+++ tools/docker-install.sh
@@ -53,7 +53,7 @@ if [ "$kernelMajor" -lt 3 ]; then
   _it_std_consoleMessage "ERROR" "La version du noyau ($kernelVersion) ne supporte pas docker. Interruption de l'installation"
   exit $FALSE
 else
-  if [ "$kernelMajor" -lt 10 ]; then
+  if [ "$kernelMinor" -lt 10 ]; then
     _it_std_consoleMessage "ERROR" "La version du noyau ($kernelVersion) ne supporte pas docker. Interruption de l'installation"
     exit $FALSE
   fi
